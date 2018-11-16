960c71cd7664a7365ce175502ce2de58903a1c55
Nate <nateschmitz17@gmail.com>
2018-11-16 13:35:42 -0600

    added GUI layout and buttons with listeners

--- src/com/nateschmitz/CalcFinal/Main2.java
+++ src/com/nateschmitz/CalcFinal/Main2.java
@@ -8,6 +8,5 @@ public class Main2 {
     public static void main(String[] args) {
     float[] calcVals = {0.0f,0.0f,0.0f};
     JOptionPane.showMessageDialog(null,"Welcome to my user interface calculator", "By: Nate Schmitz", JOptionPane.INFORMATION_MESSAGE);
-    JOptionPane.showInputDialog()w;
     }
 }
---
+++ src/com/nateschmitz/CalcFinal/calcGUI.java
@@ -0,0 +1,56 @@
+package com.nateschmitz.CalcFinal;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class calcGUI {
+    private JButton buttomMSG;
+    private JPanel panelmain;
+    private JButton a1Button;
+    private JButton a4Button;
+    private JButton a3Button;
+    private JButton a2Button;
+    boolean butt[] = {false,false,false,false};
+
+    public calcGUI() {
+        a1Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                butt[0] = true;
+                JOptionPane.showMessageDialog(null, "You clicked button 1", "Listener", JOptionPane.PLAIN_MESSAGE);
+
+
+            }
+        });
+        a2Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                butt[1] = true;
+                JOptionPane.showMessageDialog(null, "You clicked button 2","Listener", JOptionPane.PLAIN_MESSAGE);
+            }
+        });
+        a3Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                butt[2] = true;
+                JOptionPane.showMessageDialog(null, "You clicked button 3", "Listener", JOptionPane.PLAIN_MESSAGE);
+            }
+        });
+        a4Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                butt[3] = true;
+                JOptionPane.showMessageDialog(null, "You clicked button 4", "Listener", JOptionPane.PLAIN_MESSAGE);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new calcGUI().panelmain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
