f7d3cb1130b001dd381d8c4b6537c7ad74de55ad
Nate <nateschmitz17@gmail.com>
2018-11-21 00:16:34 -0600

    putting in work

--- src/com/nateschmitz/CalcFinal/calcGUI.java
+++ src/com/nateschmitz/CalcFinal/calcGUI.java
@@ -17,53 +17,111 @@ public class calcGUI {
     private JButton a7Button;
     private JButton a8Button;
     private JButton a9Button;
-    boolean butt[] = {false, false, false, false};
-    int progressVal;
+    boolean butt[] = new boolean[9];
+    public static int progressVal;
 
     public calcGUI() {
-        a1Button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                progressVal += 25;
-                butt[0] = true;
-                progressBar1.setValue(progressVal);
-                //JOptionPane.showMessageDialog(null, "You clicked button 1", "Listener", JOptionPane.PLAIN_MESSAGE);
-            }
-        });
-        a2Button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                progressVal += 25;
-                progressBar1.setValue(progressVal);
-                butt[1] = true;
-                //JOptionPane.showMessageDialog(null, "You clicked button 2","Listener", JOptionPane.PLAIN_MESSAGE);
-            }
-        });
-        a3Button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                progressVal += 25;
-                progressBar1.setValue(progressVal);
-                butt[2] = true;
-                //JOptionPane.showMessageDialog(null, "You clicked button 3", "Listener", JOptionPane.PLAIN_MESSAGE);
-            }
-        });
-        a4Button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                progressVal += 25;
-                progressBar1.setValue(progressVal);
-                butt[3] = true;
-                //JOptionPane.showMessageDialog(null, "You clicked button 4", "Listener", JOptionPane.PLAIN_MESSAGE);
-            }
-        });
-    }
+            a1Button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (butt[0] == false) {
+                        progressVal += 100 / butt.length;
+                    }
+                    butt[0] = true;
+                    progressBar1.setValue(progressVal);
+                    //JOptionPane.showMessageDialog(null, "You clicked button 1", "Listener", JOptionPane.PLAIN_MESSAGE);
+                }
+            });
+            a2Button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (butt[1] == false) {
+                        progressVal += 100 / butt.length;
+                    }
+                    progressBar1.setValue(progressVal);
+                    butt[1] = true;
+                    //JOptionPane.showMessageDialog(null, "You clicked button 2","Listener", JOptionPane.PLAIN_MESSAGE);
+                }
+            });
+            a3Button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (butt[2] == false) {
+                        progressVal += 100 / butt.length;
+                    }
+                    progressBar1.setValue(progressVal);
+                    butt[2] = true;
+                    //JOptionPane.showMessageDialog(null, "You clicked button 3", "Listener", JOptionPane.PLAIN_MESSAGE);
+                }
+            });
+            a4Button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (butt[3] == false) {
+                        progressVal += 100 / butt.length;
+                    }
+                    progressBar1.setValue(progressVal);
+                    butt[3] = true;
+                    //JOptionPane.showMessageDialog(null, "You clicked button 4", "Listener", JOptionPane.PLAIN_MESSAGE);
+                }
+            });
+            a5Button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (butt[4] == false) {
+                        progressVal += 100 / butt.length;
+                    }
+                    progressBar1.setValue(progressVal);
+                    butt[4] = true;
+                }
+            });
+            a6Button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (butt[5] == false) {
+                        progressVal += 100 / butt.length;
+                    }
+                    progressBar1.setValue(progressVal);
+                    butt[5] = true;
+                }
+            });
+            a7Button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (butt[6] == false) {
+                        progressVal += 100 / butt.length;
+                    }
+                    progressBar1.setValue(progressVal);
+                    butt[6] = true;
+                }
+            });
+            a8Button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (butt[7] == false) {
+                        progressVal += 100 / butt.length;
+                    }
+                    progressBar1.setValue(progressVal);
+                    butt[7] = true;
+                }
+            });
+            a9Button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (butt[8] == false) {
+                        progressVal += 100 / butt.length;
+                    }
+                    progressBar1.setValue(progressVal);
+                    butt[8] = true;
+                }
+            });
+        }
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("App");
-        frame.setContentPane(new calcGUI().panelmain);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+        public static void main (String[]args){
+            JFrame frame = new JFrame("App");
+            frame.setContentPane(new calcGUI().panelmain);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+        }
     }
-}
